ae359318f9b379a0ffe8119dd3d4c0d7bdf9991a
Vladimir Timofeev <vovkasm@gmail.com>
2017-08-03 09:18:30 +0300

    Wrap main in autorelease pool

--- main.mm
+++ main.mm
@@ -9,64 +9,66 @@ enum RunMode { RM_showSelected, RM_listAvailable, RM_showUsage, RM_showVersion, 
 
 int
 main(int argc, char* argv[]) {
-    // Workaround from http://lists.apple.com/archives/carbon-dev/2008/Sep/msg00009.html
-    //
-    ReceiveNextEvent(0, NULL, 0, 0, NULL);
+    @autoreleasepool {
+        // Workaround from http://lists.apple.com/archives/carbon-dev/2008/Sep/msg00009.html
+        //
+        ReceiveNextEvent(0, NULL, 0, 0, NULL);
 
-    RunMode runMode = RM_showSelected;
+        RunMode runMode = RM_showSelected;
 
-    int ch;
+        int ch;
 
-    while ((ch = getopt(argc, argv, "hlV")) != -1) {
-        switch (ch) {
-            case 'l':
-                runMode = RM_listAvailable;
-                break;
-            case 'V':
-                runMode = RM_showVersion;
-                break;
-            case 'h':
-            case '?':
-            default:
-                runMode = RM_showUsage;
-        }
-    }
-    InputSourceController ctrl;
-
-    if (optind < argc) {
-        argc -= optind;
-        argv += optind;
-        if (argc == 1) {
-            try {
-                ctrl.select(std::string(argv[0]));
-                runMode = RM_showSelected;
-            }
-            catch (program_error& e) {
-                std::cerr << e.what() << std::endl;
-                runMode = RM_showUsage;
+        while ((ch = getopt(argc, argv, "hlV")) != -1) {
+            switch (ch) {
+                case 'l':
+                    runMode = RM_listAvailable;
+                    break;
+                case 'V':
+                    runMode = RM_showVersion;
+                    break;
+                case 'h':
+                case '?':
+                default:
+                    runMode = RM_showUsage;
             }
         }
-        else {
-            std::cerr << "Error: too many arguments" << std::endl;
-            runMode = RM_showUsage;
-        }
-    }
+        InputSourceController ctrl;
 
-    switch (runMode) {
-        case RM_showSelected:
-            ctrl.showSelected();
-            break;
-        case RM_listAvailable:
-            ctrl.listAvailable();
-            break;
-        case RM_showUsage:
-            std::cout << "Usage: issw [-hlV] [<input-source-id>]" << std::endl;
-            break;
-        case RM_showVersion:
-            std::cout << "Input Source Switcher version " << InputSourceSwitcher_VERSION_MAJOR << "." << InputSourceSwitcher_VERSION_MINOR << std::endl;
-            break;
-        case RM_skip:
-            break;
+        if (optind < argc) {
+            argc -= optind;
+            argv += optind;
+            if (argc == 1) {
+                try {
+                    ctrl.select(std::string(argv[0]));
+                    runMode = RM_showSelected;
+                }
+                catch (program_error& e) {
+                    std::cerr << e.what() << std::endl;
+                    runMode = RM_showUsage;
+                }
+            }
+            else {
+                std::cerr << "Error: too many arguments" << std::endl;
+                runMode = RM_showUsage;
+            }
+        }
+
+        switch (runMode) {
+            case RM_showSelected:
+                ctrl.showSelected();
+                break;
+            case RM_listAvailable:
+                ctrl.listAvailable();
+                break;
+            case RM_showUsage:
+                std::cout << "Usage: issw [-hlV] [<input-source-id>]" << std::endl;
+                break;
+            case RM_showVersion:
+                std::cout << "Input Source Switcher version " << InputSourceSwitcher_VERSION_MAJOR << "." << InputSourceSwitcher_VERSION_MINOR << std::endl;
+                break;
+            case RM_skip:
+                break;
+        }
     }
 
     return 0;
